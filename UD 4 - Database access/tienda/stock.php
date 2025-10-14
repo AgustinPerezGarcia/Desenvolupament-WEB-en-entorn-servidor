34cb1099b7b49e1c82a9df85f1601525c631da6f
Agustin Perez Garcia <agupergar@alu.edu.gva.es>
2025-10-14 19:32:27 +0200

    Tienda, con fallo en el update

--- UD 4 - Database access/tienda/stock.php
+++ UD 4 - Database access/tienda/stock.php
@@ -8,15 +8,17 @@ $consulta = "SELECT p.nombre_corto, s.unidades, t.nombre, t.cod FROM producto p 
 $respuesta = $conexion->query($consulta);
 
 $filas = $respuesta->fetch_all();
+var_dump($filas);
 
 print "<h1>Stock del producto ".$filas[0][0]." en las tiendas:</h1>";
 
-print "<form name='actualizar' action='stock.php' method='post'>";
+print "<form name='actualizar' action='#' method='post'>";
 //print "<ul>";
+$cont = 0;
 foreach ($filas as $key => $valor) {
 
-    print "Tienda ".$valor[2].":<input name='".$valor[3]."' type='number' style='width:40px' value='".$valor[1]."'>unidades";
-
+    print "<li>Tienda ".$valor[2].":<input name='".$cont."' type='number' style='width:40px' value='".$valor[1]."'>unidades</li>";
+    $cont++;
 }
 //print "</ul>";
 
@@ -24,5 +26,22 @@ print "<input type='submit' value='Actualizar'>";
 
 print "</form>";
 
+
+$conexion->autocommit(false);
+
+try {
+    for ($i=0; $i < $cont ; $i++) { 
+        $sql = "UPDATE stock SET unidades=".$_POST[$i]." WHERE producto LIKE '".$_GET['cod']."' and tienda =".$filas[$i][3]; 
+    }
+    
+    $todo_bien = $conexion->query($sql);
+    if(!$todo_bien) throw new Exception('Error update', 1);
+    
+    $conexion->commit();
+
+} catch (Exception $e) {
+    $conexion->rollback();
+    print_r($e);
+}
 
 ?>
