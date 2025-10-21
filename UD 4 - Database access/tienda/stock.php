a3c8e09aa06849017efc5c6fefdb163854a41ec8
Agustin Perez Garcia <agupergar@alu.edu.gva.es>
2025-10-21 18:31:45 +0200

    Tienda hecha

--- UD 4 - Database access/tienda/stock.php
+++ UD 4 - Database access/tienda/stock.php
@@ -1,47 +1,63 @@
 <?php
-var_dump($_POST);
-
 
 $conexion = new mysqli('localhost', 'user', 'user', 'tienda');
 
-$consulta = "SELECT p.nombre_corto, s.unidades, t.nombre, t.cod FROM producto p JOIN stock s ON p.cod = s.producto JOIN tienda t ON t.cod = s.tienda where p.cod like '".$_GET["cod"]."'";
-$respuesta = $conexion->query($consulta);
-
-$filas = $respuesta->fetch_all();
-var_dump($filas);
-
-print "<h1>Stock del producto ".$filas[0][0]." en las tiendas:</h1>";
-
-print "<form name='actualizar' action='#' method='post'>";
-//print "<ul>";
-$cont = 0;
-foreach ($filas as $key => $valor) {
-
-    print "<li>Tienda ".$valor[2].":<input name='".$cont."' type='number' style='width:40px' value='".$valor[1]."'>unidades</li>";
-    $cont++;
-}
-//print "</ul>";
-
-print "<input type='submit' value='Actualizar'>";
-
-print "</form>";
-
-
 $conexion->autocommit(false);
 
+
+if(isset($_POST["tiendas"][0])){
+
+$nTiendas = $_POST["tiendas"][0];
+
 try {
-    for ($i=0; $i < $cont ; $i++) { 
-        $sql = "UPDATE stock SET unidades=".$_POST[$i]." WHERE producto LIKE '".$_GET['cod']."' and tienda =".$filas[$i][3]; 
+    for ($i=0; $i < $nTiendas ; $i++) { 
+        $sql = "UPDATE stock SET unidades=".$_POST["unidades"][$i]." WHERE producto LIKE '".$_GET['cod']."' and tienda = ".$_POST["tienda"][$i]; 
+
+        $todo_bien = $conexion->query($sql);
+        if(!$todo_bien) throw new Exception('Error update', 1);
     }
-    
-    $todo_bien = $conexion->query($sql);
-    if(!$todo_bien) throw new Exception('Error update', 1);
-    
+       
     $conexion->commit();
 
 } catch (Exception $e) {
     $conexion->rollback();
     print_r($e);
 }
+
+}
+
+$consulta = "SELECT p.nombre_corto, s.unidades, t.nombre, t.cod FROM producto p JOIN stock s ON p.cod = s.producto JOIN tienda t ON t.cod = s.tienda where p.cod like '".$_GET["cod"]."'";
+
+$respuesta = $conexion->query($consulta);
+
+$filas = $respuesta->fetch_all();
+
+//var_dump($filas);
+
+$cont = 0;
+
+print "<h1>Stock del producto ".$filas[0][0]." en las tiendas:</h1>";
+
+print "<form name='actualizar' action='#' method='post'>";
+//print "<ul>";
+foreach ($filas as $key => $valor) {
+
+    print "<li>Tienda ".$valor[2].":<input name='unidades[".$cont."]' type='number' style='width:40px' value='".$valor[1]."'>unidades</li>";
+
+    print "<input type='hidden' name='tienda[".$cont."]' value='".$valor[3]."'>";
+
+    $cont++;
+}
+//print "</ul>";
+
+print "<input type='hidden' name='tiendas' value='".$cont."'>";
+
+print "<input type='submit' value='Actualizar'>";
+
+print "</form>";
+
+print "<a href='index.php'><button>Volver a home</button></a>";
+
+
 
 ?>
