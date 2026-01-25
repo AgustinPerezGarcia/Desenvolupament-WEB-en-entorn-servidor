cf7faa938a118a42a2018f6c08c7869052969bfc
Agustin Perez Garcia <agupergar@alu.edu.gva.es>
2026-01-25 16:13:15 +0100

    user medio hecho

--- UD 9 - Introduction to Laravel/videoclub/routes/web.php
+++ UD 9 - Introduction to Laravel/videoclub/routes/web.php
@@ -1,19 +1,26 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\CatalogController;
-use Illuminate\Support\Facades\Route;
 
-Route::get('/', [HomeController::class, 'getHome']);
+Route::get('/', function () {
+    return view('home');
+})->middleware(['auth', 'verified'])->name('home');
 
-Route::get('/login', function (){
-    return view('auth.login');
+Route::get('/catalog', [CatalogController::class, 'getIndex'])->middleware(['auth', 'verified'])->name('catalog');
+
+Route::get('/catalog/show/{id}', [CatalogController::class, 'getShow'])->middleware(['auth', 'verified'])->name('show');
+
+Route::get('/catalog/create', [CatalogController::class, 'getCreate'])->middleware(['auth', 'verified'])->name('create');
+
+Route::get('/catalog/edit/{id}', [CatalogController::class, 'getEdit'])->middleware(['auth', 'verified'])->name('edit');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/catalog', [CatalogController::class, 'getIndex']);
-
-Route::get('/catalog/show/{id}', [CatalogController::class, 'getShow']);
-
-Route::get('/catalog/create', [CatalogController::class, 'getCreate']);
-
-Route::get('/catalog/edit/{id}', [CatalogController::class, 'getEdit']);
+require __DIR__.'/auth.php';
